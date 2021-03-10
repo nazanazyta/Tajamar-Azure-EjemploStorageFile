7d1267eeafdb9f4ae6b27fc1d7a891913adf8ad3
naza <nazari_93@hotmail.com>
2021-03-10 19:04:55 +0100

    Ejemplo en Windows Forms de Storage Files
    NuGet de WindowsAzure.Storage Deprecated

---
+++ EjemploStorageFile/Form1.Designer.cs
@@ -0,0 +1,75 @@
+﻿
+namespace EjemploStorageFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose (bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent ()
+        {
+            this.botonleerfile = new System.Windows.Forms.Button();
+            this.txtcontenido = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // botonleerfile
+            // 
+            this.botonleerfile.Location = new System.Drawing.Point(212, 28);
+            this.botonleerfile.Name = "botonleerfile";
+            this.botonleerfile.Size = new System.Drawing.Size(209, 52);
+            this.botonleerfile.TabIndex = 0;
+            this.botonleerfile.Text = "Read File Azure";
+            this.botonleerfile.UseVisualStyleBackColor = true;
+            this.botonleerfile.Click += new System.EventHandler(this.botonleerfile_Click);
+            // 
+            // txtcontenido
+            // 
+            this.txtcontenido.Location = new System.Drawing.Point(54, 107);
+            this.txtcontenido.Name = "txtcontenido";
+            this.txtcontenido.Size = new System.Drawing.Size(552, 393);
+            this.txtcontenido.TabIndex = 1;
+            this.txtcontenido.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(12F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(704, 542);
+            this.Controls.Add(this.txtcontenido);
+            this.Controls.Add(this.botonleerfile);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(6, 6, 6, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button botonleerfile;
+        private System.Windows.Forms.RichTextBox txtcontenido;
+    }
+}
+
---
+++ EjemploStorageFile/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Microsoft.Azure;
+using Microsoft.WindowsAzure.Storage;
+using Microsoft.WindowsAzure.Storage.File;
+
+namespace EjemploStorageFile
+{
+    public partial class Form1: Form
+    {
+        public Form1 ()
+        {
+            InitializeComponent();
+        }
+
+        private async void botonleerfile_Click (object sender, EventArgs e)
+        {
+            //NECESITAMOS LA CLAVE DE ACCESO A STORAGE
+            String key = CloudConfigurationManager.GetSetting("storagecnnstring");
+            //NECESITAMOS ACCEDER A TODA LA CUENTA CLOUR STORAGE
+            //PARA PODER ACCEDER A FILE
+            CloudStorageAccount account = CloudStorageAccount.Parse(key);
+            //PARA ACCEDER A CUALQUIER RECURSO, SE REALIZA MEDIANTE
+            //UN CLIENT ESPECIALIZADO
+            CloudFileClient client = account.CreateCloudFileClient();
+            //DEBEMOS ACCEDER AL RECURSO (SHARED) COMPARTIDO
+            //QUE HEMOS CREADO EN STORAGE FILE
+            CloudFileShare fileshare = client.GetShareReference("ejemplo");
+            //LOS FICHEROS LOS TENEMOS DIRECTAMENTE EN LA RAIZ DEL SHARED
+            CloudFileDirectory raiz = fileshare.GetRootDirectoryReference();
+            CloudFile file = raiz.GetFileReference("PENDIENTE.txt");
+            String contenido = await file.DownloadTextAsync();
+            this.txtcontenido.Text = contenido;
+        }
+    }
+}
